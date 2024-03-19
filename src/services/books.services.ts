b52407961e553dc06ca695617b7651d79407b93e
bitoller <bi_toller@hotmail.com>
2024-03-19 20:21:37 -0300

    feat: add books services file

---
+++ src/services/books.services.ts
@@ -0,0 +1,8 @@
+import { bookSchema } from './../schemas/book.schema';
+import { AppDataSource } from "../data-source";
+import { Book } from "../entities";
+import { AppError } from "../error";
+
+export const create = async (): Promise<void> => {};
+
+export const read = async (): Promise<void> => {};
