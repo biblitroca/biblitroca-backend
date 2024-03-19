ffbb10a5b832b1ecd0235968e074ff71172c56b8
bitoller <bi_toller@hotmail.com>
2024-03-19 20:40:09 -0300

    feat: add author to book schema

--- src/schemas/book.schema.ts
+++ src/schemas/book.schema.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const bookSchema = z.object({
   id: z.number().positive(),
   name: z.string().max(100),
+  author: z.string().max(50),
   type: z.string().max(6),
   price: z.number().positive(),
 });
