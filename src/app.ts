76d44f7ad9340514a43140e3d1655e22c53a15dd
bitoller <bi_toller@hotmail.com>
2024-03-19 20:16:13 -0300

    feat: add routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
 import "express-async-errors";
 import express, { Application } from "express";
+import { usersRouter } from "./routers/users.routers";
+import { sessionRouter } from "./routers/session.routers";
+import { booksRouter } from "./routers/books.routers";
+import { handleError } from "./middlewares/handleErrors.middleware";
 
 const app: Application = express();
 app.use(express.json());
+
+app.use("/users", usersRouter);
+
+app.use("/login", sessionRouter);
+
+app.use("/books", booksRouter);
+
+app.use(handleError);
 
 export default app;
