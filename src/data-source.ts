ff176b3b32b2bd034819a0244fe9cb4afdcb9ccd
bitoller <bi_toller@hotmail.com>
2024-03-19 20:16:26 -0300

    feat: add new imports

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,7 @@
+import "reflect-metadata";
 import { DataSource, DataSourceOptions } from "typeorm";
-import path from "path";
 import "dotenv/config";
+import path from "path";
 
 const settings = (): DataSourceOptions => {
   const entitiesPath: string = path.join(__dirname, "./entities/**.{ts,js}");
