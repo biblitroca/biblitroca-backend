ccba8fc61a4e22df7f0129f14548c6f434f9c157
bitoller <bi_toller@hotmail.com>
2024-03-23 20:22:09 -0300

    fix: wrong char count

--- src/schemas/book.schema.ts
+++ src/schemas/book.schema.ts
@@ -4,7 +4,7 @@ export const bookSchema = z.object({
   id: z.number().positive(),
   name: z.string().max(100),
   author: z.string().max(50),
-  type: z.string().max(6),
+  type: z.string().max(7),
   price: z.number().positive(),
 });
 
